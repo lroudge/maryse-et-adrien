8c3d0605da626d72c1dab4e4d257402c826daac9
lroudge <laura@hikoala.co>
2021-12-01 18:31:17 +0100

    Home page

--- components/About.tsx
+++ components/About.tsx
@@ -1,10 +1,37 @@
 import Main from './common/Main';
+import Image from 'next/image';
+import Countdown from 'react-countdown';
+
+const Completion = () => <span className="mt-3 text-l md:text-2xl">Vive les mariés!</span>;
 
 const About: React.FC = () => {
+    const countdownRenderer = ({ days, hours, minutes, seconds, completed }) => {
+      if (completed) {
+        return <Completion />;
+      } else {
+        return <span className="mt-3 text-l md:text-2xl text-emerald-700">Dans {days} jours, {hours} heures, {minutes} minutes et {seconds} secondes...</span>;
+      }
+    }
+
   return (
     <Main id="about">
-      <h1 className="text-3xl md:text-6xl font-bold font-title filter drop-shadow-md">Maryse et Adrien se marient !</h1>
-      <p className="mt-3 text-l md:text-2xl">Site en construction...</p>
+    <div className="flex flex-row">
+      <div className="flex flex-col justify-evenly items-center w-1/2">
+        <h1 className="text-3xl md:text-6xl font-bold font-title filter drop-shadow-md">Maryse et Adrien se marient !</h1>
+        <Countdown
+            date={'2022-05-07T14:00:00Z'}
+            renderer={countdownRenderer}
+        />
+      </div>
+      <div className="w-1/2">
+        <Image
+            alt="Maryse et Adrien souriants"
+            src="https://raw.githubusercontent.com/lroudge/maryse-et-adrien/main/.github/images/Onglet-Accueil.JPG"
+            width="500px"
+            height="340px"
+        />
+      </div>
+      </div>
     </Main>
   );
 };
--- next.config.js
+++ next.config.js
@@ -1,4 +1,54 @@
 /** @type {import('next').NextConfig} */
+
+const ContentSecurityPolicy = `
+	script-src 'self' 'unsafe-eval' 'unsafe-inline' *.google.com;
+	child-src *.google.com;
+	style-src 'self' 'unsafe-inline';
+	font-src 'self';
+`;
+
+const securityHeaders = [
+  {
+    key: "Content-Security-Policy",
+    value: ContentSecurityPolicy.replace(/\n/g, "")
+  },
+  {
+    key: "Referrer-Policy",
+    value: "origin-when-cross-origin"
+  },
+  {
+    key: "X-Frame-Options",
+    value: "DENY"
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff"
+  },
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on"
+  },
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=315360000; includeSubDomains; preload"
+  },
+  {
+    key: "Permissions-Policy",
+    value: "camera=(), microphone=(), geolocation=()"
+  }
+];
+
 module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/",
+        headers: securityHeaders
+      },
+    ]
+  },
+  images: {
+    domains: ['raw.githubusercontent.com'],
+  },
   reactStrictMode: true,
 }
