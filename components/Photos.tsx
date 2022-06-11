6501ee714bb3d9bc08ae310e0d994a413ae7e9cf
lroudge <laura@hikoala.co>
2022-06-11 12:53:17 +0200

    feature: add link to photos

--- components/Photos.tsx
+++ components/Photos.tsx
@@ -14,14 +14,38 @@ const Photos: React.FC = () => {
             className="rounded-md"
           />
         </div>
-        <div className="flex flex-col items-center justify-evenly md:w-1/2 h-96 md:h-full p-5 md:p-10">
+        <div className="flex flex-col items-center justify-evenly md:w-1/2 h-200 md:h-full p-5 md:p-10">
           <h1 className="text-3xl md:text-6xl font-bold font-title filter drop-shadow-md text-emerald-700">Photos</h1>
-          <span className="mt-3 text-l md:text-2xl text-emerald-700">
-            Yannick, photographe, sera parmi nous pour immortaliser l’ensemble de la journée et vous permettre de
-            profiter au mieux de ces moments.
+          <div className="flex flex-col mt-3 text-l md:text-2xl text-emerald-700 justify-evenly md:justify-center items-center">
+            Pour accéder aux photos du mariage, cliquez sur le lien suivant :
             <br />
-            Toutes les photos du mariage seront disponibles sur cette page dès qu’elles nous seront communiquées.
-          </span>
+            <br />
+            <a
+              href="https://www.yannickbilliouxphotography.com/"
+              aria-describedby="new-window"
+              className="flex flex-row items-center justify-center md:justify-evenly md:text-3xl text-emerald-800"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              yannickbilliouxphotography.com
+            </a>
+            <br />
+            <br />
+            <p>
+              Puis allez dans l’onglet <b>ACCÈS CLIENT</b> et entrez les codes suivants :
+            </p>
+            <br />
+            <br />
+            <div className="flex flex-col items-center justify-center shadow-lg rounded-md md:text-2xl bg-emerald-500 text-emerald-700 h-36 md:h-36 w-64 md:w-124 p-2">
+              <p>
+                <b>Identifiant :</b> YB59-MARYADR0705
+              </p>
+              <br />
+              <p>
+                <b>Mot de passe :</b> MARYADR0705@&
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </Main>
